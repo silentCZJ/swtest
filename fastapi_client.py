f1befd147bd160a2df9da8a9a8ccc3b6fc29667f
silentCZJ <108327857+silentCZJ@users.noreply.github.com>
2023-12-26 15:47:35 +0800

    Update fastapi_client.py

--- fastapi_client.py
+++ fastapi_client.py
@@ -1,9 +1,22 @@
 import requests
 
-url = 'http://127.0.0.1:8080/login'
-session = requests.session()
-info = {'username': 'kevin',
-        'password': '123456'}
-res = session.post(url=url,json=info)
-print(res.text)
-print(res.status_code)
+def get_token():
+    url = 'http://127.0.0.1:8080/login'
+    info = {'username': 'kevin',
+            'password': '123456'}
+    res = session.post(url=url,json=info)
+    token = res.json()['token']
+    print(token)
+    return token
+
+def verify_token(token):
+    url = 'http://127.0.0.1:8080/verify'
+    headers = {'Authorization': f'Bearer {token}'}
+    res = session.get(url=url,headers=headers)
+    print(res.json())
+    print(res.status_code)
+
+if __name__ == '__main__':
+    session = requests.session()
+    token = get_token()
+    verify_token(token)
