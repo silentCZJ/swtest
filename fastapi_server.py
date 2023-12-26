e8bfe735100162f7d102be3ee4be883cea0998b7
silentCZJ <108327857+silentCZJ@users.noreply.github.com>
2023-12-26 17:01:21 +0800

    Update fastapi_server.py

--- fastapi_server.py
+++ fastapi_server.py
@@ -13,6 +13,9 @@ class User(BaseModel):
 
 @app.post("/login")
 def login(user: User):
+    """這裡簡單使用明文進行判斷，實際上需要根據用戶名來檢索數據庫中的用戶信息，並使用加密函數對傳入的密碼進行hash運算，
+    並與數據庫中的密碼（hash值，這樣的好處是數據庫被盜也不會暴露用戶信息）進行比對。
+    """
     if user.username == 'kevin' and user.password == '123456':
         expiration_time = datetime.utcnow() + timedelta(minutes=10)
         payload = {'sub': user.username, 'exp': expiration_time}
